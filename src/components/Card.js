fae43e3a12ff9aa121f66c59b4d3ef190b5f0792
Volodymyr Baisa <biosvova@gmail.com>
2021-02-20 13:08:22 -0500

    Fix datas, add test jsx in card for information output, Challenges add code for render cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,21 @@
 import React from "react";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const Card = () => {
-    return <div></div>;
+const Card = ({ photo, user_icon, title, time, distance, playlist_items }) => {
+    return (
+        <StyledCard>
+            <img src={Object.values(photo)} />
+            <img src={Object.values(user_icon)} />
+            <div>{title}</div>
+            <div>{time}</div>
+            <div>{distance}</div>
+            <div>{playlist_items}</div>
+        </StyledCard>
+    );
 };
+
+const StyledCard = () => styled(motion.div)``;
 
 export default Card;
