0f876eb8eee77f3364459c315b169750e686ee1d
Volodymyr Baisa <biosvova@gmail.com>
2021-02-20 13:57:27 -0500

    add description to data, add router to App, show view details link in Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMemoryHistory } from "history";
 //Import Pages
 import Challenges from "./pages/Challenges";
 //Import Data
 import datas from "./data";
 
 function App() {
-    return <Challenges datas={datas} />;
+    const history = createMemoryHistory();
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route
+                    exact
+                    path="/"
+                    render={() => <Challenges datas={datas} />}
+                ></Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+//Icons
+import timerIcon from "../img/svg/icn_timer_line.svg";
+import distanceIcon from "../img/svg/icn_distance_line.svg";
+import playlistIcon from "../img/svg/icn_playlist.svg";
 
-const Card = ({ photo, user_icon, title, time, distance, playlist_items }) => {
+const Card = ({
+    photo,
+    user_icon,
+    title,
+    timer,
+    distance,
+    playlist_items,
+    details,
+}) => {
     return (
         <StyledCard>
-            <img src={Object.values(photo)} />
-            <img src={Object.values(user_icon)} />
-            <div>{title}</div>
-            <div>{time}</div>
-            <div>{distance}</div>
-            <div>{playlist_items}</div>
+            <img src={Object.values(photo)} alt="" />
+            <div className="info-wrapper">
+                <div className="title-wrapper">
+                    <img src={Object.values(user_icon)} alt="User icon" />
+                    <div>{title}</div>
+                </div>
+                <div className="timer-wrapper">
+                    <img src={timerIcon} alt="" />
+                    <div>{timer}</div>
+                </div>
+                <div>
+                    <img src={distanceIcon} alt="" />
+                    <div>{distance}</div>
+                </div>
+                <div>{playlist_items}</div>
+                {details && <Link to="/description">View Details</Link>}
+            </div>
         </StyledCard>
     );
 };
 
-const StyledCard = () => styled(motion.div)``;
+const StyledCard = styled(motion.div)``;
 
 export default Card;
