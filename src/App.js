6f73cb96ac3f07529fbc4441ad287c70b89d7052
Volodymyr Baisa <biosvova@gmail.com>
2021-02-22 12:01:22 -0500

    Move fonts from GlobalStyle to index.html, remove BrouserHistory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
 import { createMemoryHistory } from "history";
 //Import Pages
 import Challenges from "./pages/Challenges";
--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -8,8 +8,6 @@ const GlobalStyles = createGlobalStyle`
     //outline: 1px solid #66b8da !important;
 }
 
-@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap");
-
 html {
     font-size: 0.65vw;
     font-family: "Open Sans", sans-serif;
